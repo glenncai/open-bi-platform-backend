c72ca199fac820c823461ff8f289b03776470e7c
Glenn Cai <glenn.lh.cai@gmail.com>
2023-07-24 22:46:53 +0800

    feat: chart dto

---
+++ src/main/java/com/glenncai/openbiplatform/model/dto/chart/ChartEditRequest.java
@@ -0,0 +1,44 @@
+package com.glenncai.openbiplatform.model.dto.chart;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * This class is for editing chart request body
+ *
+ * @author Glenn Cai
+ * @version 1.0 07/24/2023
+ */
+@Data
+public class ChartEditRequest implements Serializable {
+
+  @Serial
+  private static final long serialVersionUID = -9092786050706527465L;
+
+  /**
+   * Chart id
+   */
+  private Long id;
+
+  /**
+   * Chart name
+   */
+  private String name;
+
+  /**
+   * Goal
+   */
+  private String goal;
+
+  /**
+   * Chart data
+   */
+  private String chartData;
+
+  /**
+   * Chart type
+   */
+  private String chartType;
+}
